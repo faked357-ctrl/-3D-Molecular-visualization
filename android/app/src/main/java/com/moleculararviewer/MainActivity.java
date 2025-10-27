840a5c4146eaf5e3bd52780a68857ad22c2d97ef
faked357-ctrl <faked357@gmail.com>
2025-10-27 18:11:25 +0300

    Add Android configuration, Gradle files, resources, and fix dependencies

---
+++ android/app/src/main/java/com/moleculararviewer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.moleculararviewer;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "MolecularARViewer";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+}
